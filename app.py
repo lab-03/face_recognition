d7b63ac57ee2543a569e419a04e49fbb9060312a
anwar <mac.mem65@gmail.com>
2020-03-01 19:44:21 +0200

    added exampe files

--- app.py
+++ app.py
@@ -9,5 +9,10 @@ def compare_images():
     return compare()
 
 
+@app.route('/compare', methods=['GET'])
+def compare_page(): 
+    return app.send_static_file('example.html')
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5001, debug=True)
