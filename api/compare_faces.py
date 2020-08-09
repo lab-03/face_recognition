2e7daf798f15c48cd101e56b3fe159d32cedd021
anwar <mac.mem65@gmail.com>
2020-08-09 15:52:31 +0200

    added accuracy to resp

--- api/compare_faces.py
+++ api/compare_faces.py
@@ -4,12 +4,13 @@ from helpers.image_validations import allowed_file
 from helpers.image_downloader import url_to_image
 
 def compare():
+    accuracy = 0
     if check_if_image_missing(): 
-       return same_person_resp(False)
+       return same_person_resp(False, 0)
     original_face_image =  get_face_image(image_url('original_face'))
     captured_face_image =  get_face_image(image_url('captured_face'))
-    same_person = compare_faces(original_face_image, captured_face_image)
-    return same_person_resp(same_person)
+    same_person, accuracy = compare_faces(original_face_image, captured_face_image)
+    return same_person_resp(same_person, accuracy)
 
 
 
@@ -20,8 +21,8 @@ def check_if_image_missing():
 def get_face_image(file_url):
     return url_to_image(file_url)
 
-def same_person_resp(status):
-    return jsonify({"same_person": status})
+def same_person_resp(status, accuracy):
+    return jsonify({"same_person": status, "same_person_accuracy": accuracy})
 
 def image_url(key):
     return request.json[key]
--- face_recgonition/face_recognition_api.py
+++ face_recgonition/face_recognition_api.py
@@ -10,9 +10,11 @@ def compare_faces(original_face, captured_face):
 
     if len(captured_face_encoding) > 0 and len(original_face_encoding) > 0:
         same_face = face_recognition.compare_faces([original_face_encoding[0]], captured_face_encoding[0])
+        accuracy = round(face_recognition.face_distance(original_face_encoding, captured_face_encoding[0])[0] * 100, 2)
         if same_face[0]:
             same_person = True
-    return same_person
+        print(accuracy);
+    return same_person, accuracy
 
 def get_image_encoding(image_file):
     image = face_recognition.load_image_file(image_file)
